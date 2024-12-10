91455449bfd794347f89359490b3a585c8515b19
Eduardofbarreto <seuemail@example.com>
2024-12-10 13:51:35 -0300

    começando os cálculos

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Calcular{
+    public int somar(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int subtrair(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int multiplicar(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public int dividir(int num1, int num2){
+        return num1/num2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+      
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um valor para num1: ");
+        float num1 = scanner.nextFloat();
+
+        System.out.print("Digite um valor para num2: ");
+        float num2 = scanner.nextFloat();
+
+    }
+}
